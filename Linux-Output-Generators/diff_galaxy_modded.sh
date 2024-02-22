59b14cff7197d38700fbcf0d6f6e72c2b4cbd944
LariVille <97411636+LariVille@users.noreply.github.com>
2024-02-22 15:01:16 +0100

    adding linux support
    adding files for linux support

---
+++ Linux-Output-Generators/diff_galaxy_modded.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Please Wait... Make Sure This Is The Modded ROM."
+while IFS= read -r -d '' f; do
+    if [ -f "$f" ] && [ -s "$f" ]; then
+        echo -n "$(realpath "$f") - " >> output2.txt
+        sha256sum "$f" | awk '{print $1}' >> output2.txt
+    elif [ -f "$f" ]; then
+        echo "An empty file was found: $f. Ignored the file."
+    fi
+done < <(find . -type f -print0)
+echo -e "\e[32mAll Done!\e[0m"
+sleep 5
+exit
---
+++ generate_linux.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+with open('differed.txt', 'r') as file:
+    paths = [line.split(' - ')[0] for line in file if line.strip()]
+
+for path in paths:
+
+    if os.path.isabs(path):
+        abs_path = path
+    else:
+        abs_path = os.path.abspath(path)
+
+    dest_path = os.path.join('GENERATED', abs_path.lstrip('/'))
+
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+
+    shutil.copy2(abs_path, dest_path)
