24bda6052c280c8c1f32903705e62806a48db628
LariVille <97411636+LariVille@users.noreply.github.com>
2024-03-13 16:56:51 +0100

    Update diff_galaxy_og.bat
    adding colors

--- Windows-Output-Generators/diff_galaxy_og.bat
+++ Windows-Output-Generators/diff_galaxy_og.bat
@@ -1,16 +1,16 @@
 @echo off
-echo Please Wait... Make Sure This Is The Original ROM.
+title Pygafi Output Generator [Original ROM]
+echo Please Wait... Make Sure This Is The [1mOriginal ROM[0m.
 for /R . %%f in (*.*) do (
     for %%s in ("%%f") do (
         if %%~zs gtr 0 (
             echo | set/p="%%f - " >> output1.txt
             certutil -hashfile "%%f" SHA256 | findstr /V ":" >> output1.txt
         ) else (
-            echo An empty file was found: %%~nxf. Ignored the file.
+            echo [33mAn empty file was found: %%~nxf. Ignored the file.[0m
         )
     )
 )
-color 0A
-echo All Done!
+echo [92mAll Done![0m
 timeout /t 5
 exit
