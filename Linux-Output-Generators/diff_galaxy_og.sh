64e13ad54c4ec6313f805a78cb5f9f3799edaed9
LariVille <97411636+LariVille@users.noreply.github.com>
2024-03-13 17:14:27 +0100

    Update diff_galaxy_og.sh

--- Linux-Output-Generators/diff_galaxy_og.sh
+++ Linux-Output-Generators/diff_galaxy_og.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "Please Wait... Make Sure This Is The Original ROM."
+echo -e "Please Wait... Make Sure This Is The \e[1mOriginal ROM\e[0m."
 while IFS= read -r -d '' f; do
     if [ -f "$f" ] && [ -s "$f" ]; then
         echo -n "$(realpath "$f") - " >> output1.txt
         sha256sum "$f" | awk '{print $1}' >> output1.txt
     elif [ -f "$f" ]; then
-        echo "An empty file was found: $f. Ignored the file."
+        echo -e "\e[1;33mAn empty file was found: $f. Ignored the file.\e[0m"
     fi
 done < <(find . -type f -print0)
 echo -e "\e[32mAll Done!\e[0m"
