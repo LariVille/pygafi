ba1f0257fc8ce03a4b31bdf9bb7d8f552e52717f
LariVille <97411636+LariVille@users.noreply.github.com>
2024-04-14 19:32:05 +0200

    Update differed_galaxy.py
    bug fix & better file optimisation

--- differed_galaxy.py
+++ differed_galaxy.py
@@ -1,9 +1,21 @@
+from collections import defaultdict
+
 with open('output1.txt', 'r') as file1, open('output2.txt', 'r') as file2:
-    hashes1 = {line.split(' - ')[1].strip(): line.split(' - ')[0] for line in file1 if 'diff_galaxy_og' not in line and 'diff_galaxy_modded' not in line}
-    hashes2 = {line.split(' - ')[1].strip(): line.split(' - ')[0] for line in file2 if 'diff_galaxy_og' not in line and 'diff_galaxy_modded' not in line}
+    hashes1 = defaultdict(list)
+    for line in file1:
+        if 'diff_galaxy_og' not in line and 'diff_galaxy_modded' not in line:
+            path, hash = line.split(' - ')[0], line.split(' - ')[1].strip()
+            hashes1[hash].append(path)
+
+    hashes2 = defaultdict(list)
+    for line in file2:
+        if 'diff_galaxy_og' not in line and 'diff_galaxy_modded' not in line:
+            path, hash = line.split(' - ')[0], line.split(' - ')[1].strip()
+            hashes2[hash].append(path)
 
 diff_hashes = set(hashes2.keys()).difference(hashes1.keys())
 
 with open('differed.txt', 'w') as outfile:
     for hash in diff_hashes:
-        outfile.write(hashes2[hash] + ' - ' + hash + '\n\n')
+        for path in hashes2[hash]:
+            outfile.write(path + ' - ' + hash + '\n\n')
