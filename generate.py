a8ae1440723ac90128af61c4e2871e6398ae43fb
LariVille <97411636+LariVille@users.noreply.github.com>
2024-04-15 18:19:20 +0200

    Update generate.py
    added an error handler

--- generate.py
+++ generate.py
@@ -1,16 +1,20 @@
 import os
 import shutil
 
-with open('differed.txt', 'r') as file:
-    paths = [line.split(' - ')[0] for line in file if line.strip()]
+try:
+    with open('differed.txt', 'r') as file:
+        paths = [line.split(' - ')[0] for line in file if line.strip()]
 
-for path in paths:
-    abs_path = os.path.abspath(path)
+    for path in paths:
+        abs_path = os.path.abspath(path)
 
-    _, rel_path = os.path.splitdrive(abs_path)
+        _, rel_path = os.path.splitdrive(abs_path)
 
-    dest_path = os.path.join('GENERATED', rel_path.lstrip('\\'))
+        dest_path = os.path.join('GENERATED', rel_path.lstrip('\\'))
 
-    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+        os.makedirs(os.path.dirname(dest_path), exist_ok=True)
 
-    shutil.copy2(path, dest_path)
+        shutil.copy2(path, dest_path)
+
+except Exception as e:
+    print(str(e))
