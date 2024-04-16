5031a3d7feb6d0508c66cf689ac0e55ed9d81d2e
LariVille <97411636+LariVille@users.noreply.github.com>
2024-04-16 13:34:30 +0200

    Update diff_galaxy_og.sh
    txt to ogfs

--- Linux-Output-Generators/diff_galaxy_og.sh
+++ Linux-Output-Generators/diff_galaxy_og.sh
@@ -2,8 +2,8 @@
 echo -e "Please Wait... Make Sure This Is The \e[1mOriginal ROM\e[0m."
 while IFS= read -r -d '' f; do
     if [ -f "$f" ] && [ -s "$f" ]; then
-        echo -n "$(realpath "$f") - " >> output1.txt
-        sha256sum "$f" | awk '{print $1}' >> output1.txt
+        echo -n "$(realpath "$f") - " >> output.ogfs
+        sha256sum "$f" | awk '{print $1}' >> output.ogfs
     elif [ -f "$f" ]; then
         echo -e "\e[1;33mAn empty file was found: $f. Ignored the file.\e[0m"
     fi
