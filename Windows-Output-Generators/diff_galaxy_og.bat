8059eee4e1fef327699511212458e28c9eaadac0
LariVille <97411636+LariVille@users.noreply.github.com>
2024-04-16 13:31:42 +0200

    Update diff_galaxy_og.bat
    transfer from .txt to .ogfs

--- Windows-Output-Generators/diff_galaxy_og.bat
+++ Windows-Output-Generators/diff_galaxy_og.bat
@@ -4,8 +4,8 @@ echo Please Wait... Make Sure This Is The [1mOriginal ROM[0m.
 for /R . %%f in (*.*) do (
     for %%s in ("%%f") do (
         if %%~zs gtr 0 (
-            echo | set/p="%%f - " >> output1.txt
-            certutil -hashfile "%%f" SHA256 | findstr /V ":" >> output1.txt
+            echo | set/p="%%f - " >> output.ogfs
+            certutil -hashfile "%%f" SHA256 | findstr /V ":" >> output.ogfs
         ) else (
             echo [33mAn empty file was found: %%~nxf. Ignored the file.[0m
         )
