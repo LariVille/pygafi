7cbe851c702a73dc9b1fa8f26ffbafa677b419f4
LariVille <97411636+LariVille@users.noreply.github.com>
2024-04-16 13:29:51 +0200

    Update differed_galaxy.py
    .ogfs and .mgfs support

--- differed_galaxy.py
+++ differed_galaxy.py
@@ -1,18 +1,25 @@
+import glob
 from collections import defaultdict
 
 try:
-    with open('output1.txt', 'r') as file1, open('output2.txt', 'r') as file2:
-        hashes1 = defaultdict(list)
-        for line in file1:
-            if 'diff_galaxy_og' not in line and 'diff_galaxy_modded' not in line:
-                path, hash = line.split(' - ')[0], line.split(' - ')[1].strip()
-                hashes1[hash].append(path)
+    files1 = glob.glob('*.ogfs')
+    files2 = glob.glob('*.mgfs')
 
-        hashes2 = defaultdict(list)
-        for line in file2:
-            if 'diff_galaxy_og' not in line and 'diff_galaxy_modded' not in line:
-                path, hash = line.split(' - ')[0], line.split(' - ')[1].strip()
-                hashes2[hash].append(path)
+    hashes1 = defaultdict(list)
+    for filename in files1:
+        with open(filename, 'r') as file:
+            for line in file:
+                if 'diff_galaxy_og' not in line and 'diff_galaxy_modded' not in line:
+                    path, hash = line.split(' - ')[0], line.split(' - ')[1].strip()
+                    hashes1[hash].append(path)
+
+    hashes2 = defaultdict(list)
+    for filename in files2:
+        with open(filename, 'r') as file:
+            for line in file:
+                if 'diff_galaxy_og' not in line and 'diff_galaxy_modded' not in line:
+                    path, hash = line.split(' - ')[0], line.split(' - ')[1].strip()
+                    hashes2[hash].append(path)
 
     diff_hashes = set(hashes2.keys()).difference(hashes1.keys())
 
