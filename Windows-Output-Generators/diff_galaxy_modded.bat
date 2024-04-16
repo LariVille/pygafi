0220451aba616ddc86ca1e11381d3db89c79be53
LariVille <97411636+LariVille@users.noreply.github.com>
2024-04-16 13:32:28 +0200

    Update diff_galaxy_modded.bat
    transfer from .txt to .mgfs

--- Windows-Output-Generators/diff_galaxy_modded.bat
+++ Windows-Output-Generators/diff_galaxy_modded.bat
@@ -4,8 +4,8 @@ echo Please Wait... Make Sure This Is The [1mModded ROM[0m.
 for /R . %%f in (*.*) do (
     for %%s in ("%%f") do (
         if %%~zs gtr 0 (
-            echo | set/p="%%f - " >> output2.txt
-            certutil -hashfile "%%f" SHA256 | findstr /V ":" >> output2.txt
+            echo | set/p="%%f - " >> output.mgfs
+            certutil -hashfile "%%f" SHA256 | findstr /V ":" >> output.mgfs
         ) else (
             echo [33mAn empty file was found: %%~nxf. Ignored the file.[0m
         )
